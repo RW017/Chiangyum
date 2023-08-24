659a615cac1c9c7f40e965677cabd80f9aee9d4c
lauchloeyun <133767158+RW017@users.noreply.github.com>
2023-08-25 00:27:54 +0800

    3

--- 2_login.php
+++ 2_login.php
@@ -11,12 +11,12 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>登入介面</title>
+<title>蔣家小館員工系統登入介面</title>
 </head>
 <body>
-    <h1>Log In</h1>
+    <h1>蔣家小館員工系統登入介面</h1>
     <h2>你可以選擇登入或是註冊帳號~</h2>
-<form method="post" action="2_login.php">
+<form method="post" action="2_sigin.php">
 帳號：
 <input type="text" name="username"><br/><br/>
 密碼：
